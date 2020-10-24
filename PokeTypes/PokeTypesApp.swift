584f39d639810f04c9c4b1c9bc80c3a938a3703a
Jaume Corbi <jaume4@gmail.com>
2020-10-24 18:10:45 +0200

    Initial Commit

---
+++ PokeTypes/PokeTypesApp.swift
@@ -0,0 +1,20 @@
+//
+//  PokeTypesApp.swift
+//  PokeTypes
+//
+//  Created by Jaume on 24/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct PokeTypesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
