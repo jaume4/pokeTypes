915553eacce504d77b548a9871a12d9ec4e3fe38
Jaume Corbi <jaume4@gmail.com>
2020-10-25 08:57:34 +0100

    static database in bundle

--- PokeTypes/ContentView.swift
+++ PokeTypes/ContentView.swift
@@ -27,6 +27,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
     }
 }
